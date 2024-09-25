ebb9ac48e2882a1756d035525c3f0b78807e3697
Zasha <104431565+zsyborg@users.noreply.github.com>
2024-09-25 07:58:55 +0530

    Improved NextJs Export Settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "",
+    assetPrefix: "",
+    trailingSlash: true,
+};
 
 export default nextConfig;
